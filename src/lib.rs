22ab3d33b4d3526f62582b6014cdf96aae7e27d3
Wojciech Graj <me@w-graj.net>
2025-10-21 11:42:26 +0200

    Fix failing docs build
    The doc_auto_cfg attribute has been merged into doc_cfg
    rust-lang/rust#43781

--- src/lib.rs
+++ src/lib.rs
@@ -1,6 +1,6 @@
 //! > Polyfill for `is_terminal` stdlib feature for use with older MSRVs
 
-#![cfg_attr(docsrs, feature(doc_auto_cfg))]
+#![cfg_attr(docsrs, feature(doc_cfg))]
 #![warn(clippy::print_stderr)]
 #![warn(clippy::print_stdout)]
 
